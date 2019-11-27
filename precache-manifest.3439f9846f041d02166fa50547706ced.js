5dc748ea51f958e3a2baf6474a33117a97140765
ThomasJRyan <tomryan_8_1997@hotmail.com>
2019-11-26 19:19:46 -0500

    Updates

---
+++ precache-manifest.3439f9846f041d02166fa50547706ced.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a5fbd6e3f61981ae3278ba1da17f930",
+    "url": "/thomasjr-website/index.html"
+  },
+  {
+    "revision": "e7cb669f790c83d6f431",
+    "url": "/thomasjr-website/static/css/main.13bd9ff4.chunk.css"
+  },
+  {
+    "revision": "38d28f4ce2a3c3b8940c",
+    "url": "/thomasjr-website/static/js/2.adf9f747.chunk.js"
+  },
+  {
+    "revision": "e7cb669f790c83d6f431",
+    "url": "/thomasjr-website/static/js/main.8e9073b5.chunk.js"
+  },
+  {
+    "revision": "e1f36f95619af5d2f35e",
+    "url": "/thomasjr-website/static/js/runtime-main.b3a41324.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thomasjr-website/precache-manifest.b1a7d54ea43e58bd6a116f0b4d7ed991.js"
+  "/thomasjr-website/precache-manifest.3439f9846f041d02166fa50547706ced.js"
 );
 
 self.addEventListener('message', (event) => {
