903bbb07b5f42a1bd216276e1af610e5ca0908ad
Thomas Ryan <tomryan_8_1997@hotmail.com>
2019-11-30 12:45:31 -0500

    Added images

--- src/Components/Blurb.js
+++ src/Components/Blurb.js
@@ -12,7 +12,7 @@ export default class Blurb extends React.Component {
               <div style={{width: "100vmin", margin: "5vmin"}}>
                 <img src="https://cdn.discordapp.com/avatars/160941453671923722/ec280069971c9c5a6970636927fa7ea5.webp?size=128" alt="ProfilePicture"/>
                 {INFORMATION.bio.map((line) => (
-                  <p>{line}</p>
+                  <div>{line}</div>
                 ))}
               </div>
             </div>
