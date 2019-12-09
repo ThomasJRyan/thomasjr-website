4aa647b996264993d79aa0cb025d582a22cda56a
ThomasJRyan <tomryan_8_1997@hotmail.com>
2019-12-09 00:08:33 -0500

    Removed blurb image, changed skill transistion time

--- src/Components/Blurb.js
+++ src/Components/Blurb.js
@@ -10,7 +10,6 @@ export default class Blurb extends React.Component {
             <h1>About Me</h1>
             <div className="Blurb-div">
               <div style={{width: "100vmin", margin: "5vmin"}}>
-                <img src="https://cdn.discordapp.com/avatars/160941453671923722/ec280069971c9c5a6970636927fa7ea5.webp?size=128" alt="ProfilePicture"/>
                 {INFORMATION.bio.map((line) => (
                   <p>{line}</p>
                 ))}
